c193d0851a8300fe24c38a4b54008f5411743f60
chithien-day <thientran.080901@gmail.com>
2023-03-30 02:20:51 +0700

    added swagger for signin & signup

--- 1_API/app/routes/auth.routes.js
+++ 1_API/app/routes/auth.routes.js
@@ -1,3 +1,94 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Authentications:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the accounts
+ *         username:
+ *           type: string
+ *           description: The title of your account
+ *         email:
+ *           type: string
+ *           description: The title of your email
+ *         password:
+ *           type: strign
+ *           description: The title of your password
+ *       example:
+ *         username: Genshin
+ *         email: Genshin
+ *         password: Aa@123
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Authentications
+ *   description: The Athentication managing API
+ * /auth/signin:
+ *   post:
+ *     summary: Login to your account
+ *     tags: [Authentications]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Authentications'
+ *     responses:
+ *       200:
+ *         description: Login successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Authentications'
+ *       500:
+ *         description: Some server error
+ * /auth/adsignup:
+ *   post:
+ *     summary: Signup to the admin account
+ *     tags: [Authentications]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Authentications'
+ *     responses:
+ *       200:
+ *         description: Login successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Authentications'
+ *       500:
+ *         description: Some server error
+ * /auth/signup:
+ *   post:
+ *     summary: Signup the user account
+ *     tags: [Authentications]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Authentications'
+ *     responses:
+ *       200:
+ *         description: Login successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Authentications'
+ *       500:
+ *         description: Some server error
+ */
+
 const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 
