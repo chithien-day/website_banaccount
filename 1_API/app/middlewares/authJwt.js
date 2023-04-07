15a7e9c235b5c441a9febf8037e21b43c0832a0f
chithien-day <thientran.080901@gmail.com>
2023-04-07 20:45:47 +0700

    fix author

--- 1_API/app/middlewares/authJwt.js
+++ 1_API/app/middlewares/authJwt.js
@@ -4,87 +4,99 @@ const db = require("../models");
 const User = db.user;
 const Role = db.role;
 
-verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"];
 
-  if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
-  }
-
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized!" });
+    if (!token) {
+      return res.status(403).send({ message: "No token provided!" });
     }
+
+    const decoded = await jwt.verify(token, config.secret);
     req.userId = decoded.id;
     next();
-  });
+  } catch (err) {
+    return res.status(401).send({ message: "Unauthorized!" });
+  }
 };
 
-isAdmin = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
+isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId).exec();
+    const roles = await Role.find({ _id: { $in: user.roles } }).exec();
 
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
-        for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "admin") {
-            next();
-            return;
-          }
-        }
-
-        res.status(403).send({ message: "Require Admin Role!" });
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "admin") {
+        next();
         return;
       }
-    );
-  });
-};
-
-isModerator = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
     }
 
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
+    res.status(403).send({ message: "Require Admin Role!" });
+    return;
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
 
-        for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "moderator") {
-            next();
-            return;
-          }
-        }
+isModerator = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId).exec();
+    const roles = await Role.find({ _id: { $in: user.roles } }).exec();
 
-        res.status(403).send({ message: "Require Moderator Role!" });
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "moderator") {
+        next();
         return;
       }
-    );
-  });
+    }
+
+    res.status(403).send({ message: "Require Moderator Role!" });
+    return;
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
 };
+
+function checkRole(role) {
+  return (req, res, next) => {
+    const token = req.headers.authorization;
+    
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+      
+      if (decoded.role !== role) {
+        return res.status(403).json({ message: 'Forbidden' });
+      }
+      next();
+    });
+  };
+}
+// isUser = async (req, res, next) => {
+//   try {
+//     const user = await User.findById(req.userId).exec();
+//     const roles = await Role.find({ _id: { $in: user.roles } }).exec();
+
+//     for (let i = 0; i < roles.length; i++) {
+//       if (roles[i].name === "user") {
+//         next();
+//         return;
+//       }
+//     }
+
+//     res.status(403).send({ message: "Require Role!" });
+//     return;
+//   } catch (err) {
+//     res.status(500).send({ message: err });
+//   }
+// };
 
 const authJwt = {
   verifyToken,
   isAdmin,
-  isModerator
+  isModerator,
+  // isUser,
 };
 module.exports = authJwt;
