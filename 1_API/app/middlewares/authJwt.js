3b6ce5b2c8159f2d3d398ade5fefe7a6b958d239
chithien-day <thientran.080901@gmail.com>
2023-04-08 03:19:19 +0700

    add button edit game

--- 1_API/app/middlewares/authJwt.js
+++ 1_API/app/middlewares/authJwt.js
@@ -6,8 +6,8 @@ const Role = db.role;
 
 verifyToken = async (req, res, next) => {
   try {
-    const token = req.headers["x-access-token"];
-    //const token = req.headers.authorization.split(' ')[1];
+    //const token = req.headers["x-access-token"];
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       return res.status(403).send({ message: "No token provided!" });
     }
